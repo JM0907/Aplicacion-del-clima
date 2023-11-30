90408ccf83faf17aa0a60ac3437c4b79c10b3555
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-11-29 20:03:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-|from flask import Flask,render_template
+from flask import Flask,render_template
 import requests
 from dotenv import load_dotenv,dotenv_values
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
